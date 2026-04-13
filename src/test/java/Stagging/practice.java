002aad9a8fa962c3f4b292e9583dcc332e713118
sowja <sowja@BOOK-DH1J4U3JJJ>
2026-04-13 10:22:02 +0530

    i"m a expert automation test engineer

--- src/test/java/Stagging/practice.java
+++ src/test/java/Stagging/practice.java
@@ -3,6 +3,6 @@ package Stagging;
 public class practice {
 public static void main(String[] args) {
 	System.out.println("practice 2");
-	System.out.println("i am a rich girl and expert automation test engineer ");
+	System.out.println("I'm a expert automation test engineer");
 }
 }
