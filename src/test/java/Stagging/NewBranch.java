23705fb70d801ef892d14eae3eac64a58929aa5e
sowja <sowja@BOOK-DH1J4U3JJJ>
2026-04-13 12:00:25 +0530

    from SE

--- src/test/java/Stagging/NewBranch.java
+++ src/test/java/Stagging/NewBranch.java
@@ -4,5 +4,6 @@ public class NewBranch {
 public static void main(String[] args) {
 	System.out.println("I'm a aoutmation test engineer expert");
 	System.out.println("Hello");
+	System.out.println("what are you doing");
 }
 }
