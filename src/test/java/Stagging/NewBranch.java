4bc08cfa1847f7c97a446419871529375c4639bf
sowja <sowja@BOOK-DH1J4U3JJJ>
2026-04-13 11:38:03 +0530

    from ATE

--- src/test/java/Stagging/NewBranch.java
+++ src/test/java/Stagging/NewBranch.java
@@ -3,5 +3,6 @@ package Stagging;
 public class NewBranch {
 public static void main(String[] args) {
 	System.out.println("I'm a aoutmation test engineer expert");
+	System.out.println("Hello");
 }
 }
