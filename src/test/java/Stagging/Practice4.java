5de6401f7104f050816c33e8b8d41b89c742111a
sowja <sowja@BOOK-DH1J4U3JJJ>
2026-04-13 10:00:23 +0530

    I go a job in autmation with good lpa

--- src/test/java/Stagging/Practice4.java
+++ src/test/java/Stagging/Practice4.java
@@ -1,7 +1,7 @@
 package Stagging;
 
 public class Practice4 {
-	public static void main(String[] args) {
-		System.out.println("practice4");
-	}
+public static void main(String[] args) {
+	System.out.println("sow");
+}
 }
